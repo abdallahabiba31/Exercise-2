167927f2fcb46f145475daf19f44bef609742f98
abdallahabiba31 <abdallahabiba97@gmail.com>
2023-04-09 14:52:14 +0200

    movieCell added labels

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
@@ -11,11 +11,14 @@ import javafx.scene.paint.Color;
 
 public class MovieCell extends ListCell<Movie> {
     private final Label title = new Label();
-    private final Label detail = new Label();
-    //* Label für die Genre hinzugefügt
+    private final Label detailDesc = new Label();
     private final Label detailGenre = new Label();
-    //*Genre im Layout hinzugefügt
-    private final VBox layout = new VBox(title, detail, detailGenre);
+    //TODO
+    private final Label detailRating = new Label();
+    private final Label detailReleaseYear = new Label();
+
+
+    private final VBox layout = new VBox(title, detailDesc, detailGenre, detailRating, detailReleaseYear);
 
     @Override
     protected void updateItem(Movie movie, boolean empty) {
@@ -28,30 +31,43 @@ public class MovieCell extends ListCell<Movie> {
         } else {
             this.getStyleClass().add("movie-cell");
             title.setText(movie.getTitle());
-            detail.setText(
+            detailDesc.setText(
                     movie.getDescription() != null
                             ? movie.getDescription()
                             : "No description available"
             );
-            //*damit auch die Genre angezeigt wird
             detailGenre.setText(
                     movie.getGenre() != null
                             ? movie.getGenre().toString()
                             : "No Genre available"
             );
+            //TODO
+            detailRating.setText(
+                    movie.getRating() != 0
+                            ? String.format("%.2f", movie.getRating())
+                            : "No Rating available"
+            );
+            detailReleaseYear.setText(
+                    movie.getReleaseYear() != 0
+                            ? Integer.toString(movie.getReleaseYear())
+                            : "No ReleaseYear available"
+            );
 
 
             // color scheme
             title.getStyleClass().add("text-yellow");
-            detail.getStyleClass().add("text-white");
+            detailDesc.getStyleClass().add("text-white");
             //*style auch für genre hinzugefügt --> ohne dem, wäre die Textfarbe grau
             detailGenre.getStyleClass().add("text-white");
+            //TODO
+            detailReleaseYear.getStyleClass().add("text-white");
+            detailRating.getStyleClass().add("text-white");
             layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
 
             // layout
             title.fontProperty().set(title.getFont().font(20));
-            detail.setMaxWidth(this.getScene().getWidth() - 30);
-            detail.setWrapText(true);
+            detailDesc.setMaxWidth(this.getScene().getWidth() - 30);
+            detailDesc.setWrapText(true);
             layout.setPadding(new Insets(10));
             layout.spacingProperty().set(10);
             layout.alignmentProperty().set(javafx.geometry.Pos.CENTER_LEFT);
