02b4ce99593dda424ac5f5c580aa103c72120d88
abdallahabiba31 <abdallahabiba97@gmail.com>
2023-04-10 12:12:56 +0200

    added "opens at.ac.fhcampuswien.fhmdb.models to com.google.gson;" --> important!!

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,5 +7,7 @@ module at.ac.fhcampuswien.fhmdb {
     requires com.google.gson;
 
     opens at.ac.fhcampuswien.fhmdb to javafx.fxml;
+    //Ohne der unteren Zeile geht nix!!
+    opens at.ac.fhcampuswien.fhmdb.models to com.google.gson;
     exports at.ac.fhcampuswien.fhmdb;
 }
